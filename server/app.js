140654e504bb542cd78010b35d83feac17955b8e
Phouthalang Pygnasak <pygnasak@gmail.com>
2019-01-18 11:50:45 -0800

    setup unit test for user repo

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
+// APP => ROUTES => SERVICE => REPOSITORY + ENTITIES
+
 // npm packages
 const express = require("express");
 const passport = require('passport');
 const bodyParser = require('body-parser');
-
-// APP => ROUTES => SERVICE => REPOSITORY + ENTITIES
 
 // instance of express app
 const app = express();
@@ -25,20 +25,4 @@ app.use(passport.initialize());
 require('./routes/index.routes')(app);
 
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static('client/build'));
-
-    // serve index.html if route is not recognized
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const app = require('./app');
+
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+
+    // serve index.html if route is not recognized
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
